3101e382294645a523e6defb8bdd79cb1585022f
iftikhar rasha <iftikhar.rasha@northsouth.edu>
2023-03-26 22:55:48 +0600

    index.jsx

--- index.jsx
+++ index.jsx
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-        await client.connect();
+        client.connect();
 
         const database = client.db("E24Games");
         const CHAT_BOT = 'ChatBot';
